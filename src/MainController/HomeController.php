da345e8fd091be7be3db34a096972bdb7f0bbfcb
Karol Bialkowski <k.bialkowski10@gmail.com>
2019-12-20 20:02:09 +0100

    Inital commit

---
+++ src/MainController/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\MainController;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+
+    public function index()
+    {
+        return new Response('honepage');
+    }
+
+}
